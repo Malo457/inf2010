0b9746d321a8cc4ef93c283bd79859656542cd9f
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2021-09-10 10:09:50 -0400

    Mat: PointOperator finit pas teste

--- TP1/TP1/src/tp1/PointOperator.java
+++ TP1/TP1/src/tp1/PointOperator.java
@@ -13,18 +13,31 @@ public final class PointOperator {
 
     }
 
-    // TODO appliquer la translation sur le vecteur d'entree.
+    // TODO appliquer la rotation sur le vecteur d'entree.
     public static void rotate(Double[] vector, Double[][] rotationMatrix) {
 
-        // ...
+        if (vector.length != rotationMatrix[1].length) return; //exit function if multiplication is not valid
+
+        for(int firstDimension = 0; firstDimension < vector.length; firstDimension++) {
+            double result = 0;
+            for(int secondDimension = 0; secondDimension < rotationMatrix[1].length; secondDimension++) {
+                result += vector[secondDimension] * rotationMatrix[firstDimension][secondDimension];
+            }
+            vector[firstDimension] = result;
+
+        }
+
     }
 
-    // TODO appliquer la translation sur le vecteur d'entree.
+    // TODO appliquer la division sur le vecteur d'entree.
     public static void divide(Double[] vector, Double divider) {
-        // ...
+
+        for(int dimension = 0; dimension < vector.length; dimension++) {
+            vector[dimension] /= divider;
+        }
     }
 
-    // TODO appliquer la translation sur le vecteur d'entree.
+    // TODO appliquer la multiplication sur le vecteur d'entree.
     public static void multiply(Double[] vector, Double multiplier) {
 
         for(int dimension = 0; dimension < vector.length; dimension++) {
@@ -33,8 +46,11 @@ public final class PointOperator {
 
     }
 
-    // TODO appliquer la translation sur le vecteur d'entree.
+    // TODO appliquer laddition sur le vecteur d'entree.
     public static void add(Double[] vector, Double adder) {
-        // ...
+
+        for(int dimension = 0; dimension < vector.length; dimension++) {
+            vector[dimension] += adder;
+        }
     }
 }
