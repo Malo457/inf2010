3116921088ce0b7527e18c0e1ab06a58031035c7
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2021-09-10 10:44:11 -0400

    Mat: pas mal fini reste les tests

--- TP1/TP1/src/tp1/Point2d.java
+++ TP1/TP1/src/tp1/Point2d.java
@@ -6,12 +6,12 @@ public class Point2d extends AbstractPoint {
 
     // TODO creer un point en 2d avec 2 donnees
     public Point2d(Double x, Double y) {
-        super(null /*...*/);
+        super(new Double[]{x, y} );
     }
 
     // TODO creer un point a partir d'un vecteur de donnees
     public Point2d(Double[] vector) {
-        super(null /*...*/);
+        super(vector.clone());
     }
 
     public Double X() { return vector[X];}
@@ -20,46 +20,60 @@ public class Point2d extends AbstractPoint {
     // TODO appliquer la transalation sur la classe courrante et retourner la reference
     @Override
     public Point2d translate(Double[] translateVector) {
-        return null;
+        PointOperator.translate(this.vector, translateVector);
+        return this;
     }
 
     // TODO appliquer la transalation sur la classe courrante et retourner la reference
     public Point2d translate(Point2d translateVector) {
-        return null;
+        return this.translate(translateVector.vector);
     }
 
     // TODO appliquer la rotation sur la classe courrante et retourner la reference
     @Override
     public Point2d rotate(Double[][] rotationMatrix) {
-        return null;
+        PointOperator.rotate(this.vector, rotationMatrix);
+        return this;
     }
 
     // TODO appliquer la rotation sur la classe courrante et retourner la reference
     public Point2d rotate(Double angle) {
-        return null;
+        double x = this.vector[this.X];
+        double y = this.vector[this.Y];
+
+        double newX = x * Math.cos(angle) - y * Math.sin(angle);
+        double newY = x * Math.sin(angle) + y * Math.cos(angle);
+
+        this.vector[this.X] = newX;
+        this.vector[this.Y] = newY;
+
+        return this;
     }
 
     // TODO appliquer la division sur la classe courrante et retourner la reference
     @Override
     public Point2d divide(Double divider) {
-        return null;
+        PointOperator.divide(this.vector, divider);
+        return this;
     }
 
     // TODO appliquer la multiplication sur la classe courrante et retourner la reference
     @Override
     public Point2d multiply(Double multiplier) {
-        return null;
+        PointOperator.multiply(this.vector, multiplier);
+        return this;
     }
 
     // TODO appliquer la addition sur la classe courrante et retourner la reference
     @Override
     public Point2d add(Double adder) {
-        return null;
+        PointOperator.add(this.vector, adder);
+        return this;
     }
 
     // TODO creer un nouveau point.
     @Override
     public Point2d clone() {
-        return this;
+        return new Point2d(this.X(), this.Y());
     }
 }
