a31f9c61920a98880dd054beb95c1bfd0e50dd94
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2021-09-11 13:37:30 -0400

    Mat: casiment fini ellipse

--- TP1/TP1/src/tp1/Ellipse.java
+++ TP1/TP1/src/tp1/Ellipse.java
@@ -5,32 +5,31 @@ import java.util.Collection;
 public class Ellipse extends BaseShape {
     // TODO creer une ellipse avec une largeur et une longueur.
     public Ellipse(Double widthRadius, Double heightRadius) {
-
+        //super(new Point2d(widthRadius, heightRadius));
     }
 
-    // TODO creer une ellipse avec les dimensions contenu dans un Point.
     public Ellipse(Point2d dimensions) {
-        // ...
+        this(dimensions.X(), dimensions.Y());
     }
 
-    // TODO initialiser le parent.
     private Ellipse(Collection<Point2d> coords) {
-        // ...
+        super(coords);
     }
 
-    // TODO appliquer la translation sur la forme.
     @Override
     public Ellipse translate(Point2d point) {
-        return null;
+        super.translate(point);
+        return this;
     }
 
-    // TODO appliquer la rotation sur la forme.
     @Override
     public Ellipse rotate(Double angle) {
-        return null;
+        super.rotate(angle);
+        return this;
     }
 
-    // TODO retourner une nouvelle forme.
     @Override
-    public Ellipse clone() { return null; }
+    public Ellipse clone() {
+        return new Ellipse(this.getCoordsDeepCopy());
+    }
 }
