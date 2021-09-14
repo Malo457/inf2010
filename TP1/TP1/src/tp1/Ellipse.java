54b9d3b5e2eb2a8d792bd397521fc59db55f23b3
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2021-09-14 19:58:41 -0400

    Mat: des tests dellipses

--- TP1/TP1/src/tp1/Ellipse.java
+++ TP1/TP1/src/tp1/Ellipse.java
@@ -6,13 +6,34 @@ import java.util.Collection;
 public class Ellipse extends BaseShape {
 
     public Ellipse(Double widthRadius, Double heightRadius) {
-        super();
+        /*super();
         widthRadius /= (2.0);
         heightRadius /= (2.0);
-        for (int degre = 0; degre <= 360; degre++) {
-            double x = widthRadius * Math.sin(Math.toRadians(degre));
-            double y = heightRadius * Math.cos(Math.toRadians(degre));
-            this.add(new Point2d(x, y));
+
+        Collection<Point2d> newCoords = new ArrayList();
+        for (int i = 0; i < widthRadius; i++) {
+            for (int degre = 0; degre <= 360; degre++) {
+                double x = (widthRadius - i) * Math.sin(Math.toRadians(degre));
+                double y = (heightRadius - i) * Math.cos(Math.toRadians(degre));
+                Point2d newCoord = new Point2d(x, y);
+                if (!newCoords.contains(newCoord)) newCoords.add(new Point2d(x, y));
+            }
+        }
+        this.addAll(newCoords);*/
+
+        super(new ArrayList());
+        double largeur = -widthRadius / 2.0D;
+        double hauteur = -heightRadius / 2.0D;
+
+        for(double i = largeur; i < -1.0D * largeur; ++i) {
+            for(double j = hauteur; j < -1.0D * hauteur; ++j) {
+                double evolutionX = Math.pow(i, 2.0D) / Math.pow(largeur, 2.0D);
+                double evolutionY = Math.pow(j, 2.0D) / Math.pow(hauteur, 2.0D);
+                double Xy = evolutionX + evolutionY;
+                if (Xy <= 1.0D) {
+                    this.add(new Point2d(i, j));
+                }
+            }
         }
     }
 
